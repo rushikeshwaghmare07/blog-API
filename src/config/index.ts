48caf01526b529d035eb1b26004c90d5f83dfe42
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-06-20 23:24:22 +0530

    Implement CORS setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 const config = {
   PORT: process.env.PORT || 3000,
+  NODE_ENV: process.env.NODE_ENV,
 };
 
 export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,28 @@
 import express from "express";
+import cors from "cors";
 
 import config from "@/config";
 
+import type { CorsOptions } from "cors";
+
 const app = express();
 const PORT = config.PORT;
+
+const CorsOptions: CorsOptions = {
+  origin(origin, callback) {
+    if (config.NODE_ENV === "development" || !origin) {
+      callback(null, true);
+    } else {
+      callback(
+        new Error(`CORS error: ${origin} is not allowed by CORS`),
+        false,
+      );
+      console.log(`CORS error: ${origin} is not allowed by CORS`);
+    }
+  },
+};
+
+app.use(cors(CorsOptions));
 
 app.listen(PORT, () => {
   console.log(`Server running: http://localhost:${PORT}`);
