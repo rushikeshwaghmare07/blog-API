c9302ee84f1e2a8fbc5ccab16fa71a0ed8f4b2ec
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-06-20 23:58:08 +0530

    add core middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import helmet from "helmet";
 
 import config from "@/config";
+import limiter from "@/lib/express_rate_limit";
 
 import type { CorsOptions } from "cors";
 
@@ -22,8 +26,29 @@ const CorsOptions: CorsOptions = {
   },
 };
 
+// Middlewares
 app.use(cors(CorsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  compression({
+    threshold: 1024,
+  }),
+);
+app.use(helmet());
+app.use(limiter);
 
-app.listen(PORT, () => {
-  console.log(`Server running: http://localhost:${PORT}`);
-});
+(async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server running: http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log("Failed to start the server: ", error);
+
+    if (config.NODE_ENV === "production") {
+      process.exit(1);
+    }
+  }
+})();
