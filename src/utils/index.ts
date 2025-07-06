f10048da8f4dd3f54c3cf4c1c6fefed9e0e3db81
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-07-06 18:44:50 +0530

    Add utility function for generating slugs

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,3 +6,18 @@ export const genUsername = (): string => {
 
   return username;
 };
+
+// Generate a random slug from a title (e.g.: my-title-abc123)
+export const genSlug = (title: string): string => {
+  const slug = title
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z1-9]\s-/g, "")
+    .replace(/\+/g, "-")
+    .replace(/-+/g, "-");
+
+  const randomChars = Math.random().toString(36).slice(2);
+  const uniqueSlug = `${slug}-${randomChars}`;
+
+  return uniqueSlug;
+};
