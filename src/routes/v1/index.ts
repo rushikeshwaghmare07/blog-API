a8866c90d655474030cdf06ebd2132faaf595149
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-07-06 18:48:00 +0530

    Register blog routes in main API router

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -3,6 +3,7 @@ const router = Router();
 
 import authRoutes from "@/routes/v1/auth";
 import userRoutes from "@/routes/v1/user";
+import blogRoutes from "@/routes/v1/blog";
 
 // Root route
 router.get("/", (req, res) => {
@@ -16,5 +17,6 @@ router.get("/", (req, res) => {
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
+router.use("/blogs", blogRoutes);
 
 export default router;
